d29f474bafe77760f3278b7f764e91d669a07bb5
PhoebeVermithrax <phoebe.vermithrax@ocsbstudent.ca>
2018-03-06 13:14:06 -0500

    Updates
    Added listbox
    Updated code

--- src/MultiDimensionalArrays.java
+++ src/MultiDimensionalArrays.java
@@ -17,6 +17,7 @@ import org.eclipse.swt.events.SelectionEvent;
 //import the random class.
 import java.util.Random;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
 
 public class MultiDimensionalArrays {
 
@@ -50,50 +51,6 @@ public class MultiDimensionalArrays {
 		}
 	}
 	
-	//Create a function to calculate the average of the class, which takes a multi-dimensional array.
-	public int CalculateAverage(int[] []tmpArrayOfIntegers)
-	{
-		//Set the variables for the counter for both rows, the random number, the sum, and the total.
-		int countOne;
-		int countTwo;
-		int sum = 0;
-		int total = 0;
-		int randomNumber;
-		final int MAX_RANDOM_NUMBER = 99;
-		final int MIN_NUMBER = 1;
-		
-		//Generate a random number, and state the min and the max. [start + (end - start) * .nextDouble()] This is for
-		//doubles.
-		//Random randomNumberGenerator = new Random();
-		//randomNumber = MIN_NUMBER + (MAX_RANDOM_NUMBER - MIN_NUMBER) * randomNumberGenerator.nextDouble();
-		randomNumber = ((int) (Math.random() * MAX_RANDOM_NUMBER)) + MIN_NUMBER;
-		
-		//Use a nested for loop to populate the array.
-		for (countOne = 0; countOne < tmpArrayOfIntegers.length; countOne++)
-		{
-			
-			for (countTwo = 0; countTwo < tmpArrayOfIntegers[countOne].length; countTwo++)
-			{
-				//Assign the random number to the counter in the array.
-				tmpArrayOfIntegers[countOne][countTwo] = randomNumber;
-			}
-		}
-		
-		//Use a nested for loop to get the sum.
-		for (countOne = 0; countOne < tmpArrayOfIntegers.length; countOne++)
-		{
-			for (countTwo = 0; countTwo < tmpArrayOfIntegers.length; countTwo++)
-			{
-				sum += tmpArrayOfIntegers[countOne][countTwo];
-			}
-		}
-		
-		//Calculate the average.
-		total = sum / tmpArrayOfIntegers.length;
-		
-		//Return the value.
-		return total;
-	}
 
 	/**
 	 * Create contents of the window.
@@ -104,10 +61,13 @@ public class MultiDimensionalArrays {
 		shell.setText("Multi-Dimensional Arrays");
 		
 		Label lblNewLabel = new Label(shell, SWT.NONE);
-		lblNewLabel.setBounds(167, 152, 55, 15);
+		lblNewLabel.setBounds(143, 236, 55, 15);
 		lblNewLabel.setText("New Label");
 		//Make invisible.
 		lblNewLabel.setVisible(false);
+		
+		List lstAverage = new List(shell, SWT.BORDER);
+		lstAverage.setBounds(87, 92, 234, 138);
 		
 		Button btnCalculate = new Button(shell, SWT.NONE);
 		btnCalculate.addSelectionListener(new SelectionAdapter() {
@@ -128,6 +88,84 @@ public class MultiDimensionalArrays {
 				lblNewLabel.setText("" + average);
 				
 			}
+			
+			//Create a function to calculate the average of the class, which takes a multi-dimensional array.
+			public int CalculateAverage(int[] []tmpArrayOfIntegers)
+			{
+				//Set the variables for the counter for both rows, the random number, the sum, and the total.
+				int countOne;
+				int countTwo;
+				int sum = 0;
+				int total = 0;
+				int randomNumber;
+				final int MAX_RANDOM_NUMBER = 100;
+				final int MIN_NUMBER = 1;
+				
+				//Use a nested for loop to populate the array.
+				for (countOne = 0; countOne < tmpArrayOfIntegers.length; countOne++)
+				{
+					
+					for (countTwo = 0; countTwo < tmpArrayOfIntegers[countOne].length; countTwo++)
+					{
+						//Generate a random number, and state the min and the max. [start + (end - start) * .nextDouble()] This is for
+						//doubles.
+						randomNumber = ((int) (Math.random() * MAX_RANDOM_NUMBER)) + MIN_NUMBER;
+						
+						//Assign the random number to the counter in the array.
+						tmpArrayOfIntegers[countOne][countTwo] = randomNumber;
+						
+						//Set the student to 1.
+						int student = 1;
+						
+						//If the first counter is 0,
+						if (countOne == 0)
+						{
+							//The student # is 1.
+							student = 1;
+						}
+						//Or 1,
+						else if (countOne == 1)
+						{
+							//The student # is 2.
+							student = 2;
+						}
+						//Or 2,
+						else if (countOne == 2)
+						{
+							//The student # is 3.
+							student = 3;
+						}
+						//Or 3.
+						else if (countOne == 3)
+						{
+							//The student # is 4.
+							student = 4;
+						}
+						
+						//Populate the list box with the student number and their grade from the tmpArray at the 1st and
+						//second counter.
+						lstAverage.add("Student: # " + student + " Grade: " + tmpArrayOfIntegers[countOne][countTwo]);
+						
+						System.out.println(randomNumber);
+					}
+				}
+				
+				//Use a nested for loop to get the sum.
+				for (countOne = 0; countOne < tmpArrayOfIntegers.length; countOne++)
+				{
+					for (countTwo = 0; countTwo < tmpArrayOfIntegers[countOne].length; countTwo++)
+					{
+						sum += tmpArrayOfIntegers[countOne][countTwo];
+					}
+				}
+				
+				//Calculate the average.
+				total = sum / 24;
+				
+				//Return the value.
+				return total;
+			}
+			
 		});
 		btnCalculate.setBounds(116, 61, 182, 25);
 		btnCalculate.setText("Calculate");
@@ -137,7 +175,7 @@ public class MultiDimensionalArrays {
 		lblClickTheButton.setText("Click the button to populate an array and get the average.");
 		
 		Label lblTheAverageIs = new Label(shell, SWT.NONE);
-		lblTheAverageIs.setBounds(49, 152, 88, 15);
+		lblTheAverageIs.setBounds(49, 236, 88, 15);
 		lblTheAverageIs.setText("The average is:");
 		
 
